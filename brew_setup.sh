b27feb6c1cb02f47be15b40377ba5c3e41bd82d9
Roberto Efraín Díaz <32281616+rediaz111@users.noreply.github.com>
2018-04-22 12:03:35 -0700

    Update brew_setup.sh
    /Science and /Dupes are both deprecated. Replaced with brewsci/bio

--- brew_setup.sh
+++ brew_setup.sh
@@ -5,6 +5,10 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew tap caskroom/cask
 
 # Tap dupes and science... for science
-brew tap homebrew/science
-brew tap homebrew/dupes
+#brew tap homebrew/science
+#brew tap homebrew/dupes
+#/science and /dupes are both deprecated.
+
+brew tap brewsci/bio
+
 brew tap caskroom/versions
